21d6737c89e7a261fbc2226010831399c0830100
samih <samih@ustadmobile.com>
2021-10-13 13:47:51 +0400

    change to java

--- common/src/main/java/com/turn/ttorrent/common/TorrentLoggerFactory.java
+++ common/src/main/java/com/turn/ttorrent/common/TorrentLoggerFactory.java
@@ -1,11 +1,6 @@
 package com.turn.ttorrent.common;
 
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 
 public final class TorrentLoggerFactory {
 
